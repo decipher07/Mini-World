9b6e4a0db7e0726bdcd0a92472d361c8cd748fec
DJ <social.deej@gmail.com>
2020-09-02 01:59:18 +0530

    Getting The Access Token

--- src/spotify.js
+++ src/spotify.js
@@ -14,6 +14,15 @@ const scopes = [
 ];
 
 // Getting The Access Token
+export const getTokenFromUrl = () => {
+    return window.location.hash.substring(1).split("&")
+    .reduce((initial, item) => {
+        var parts = item.split("=")
+        initial[parts[0]] = decodeURIComponent(parts[1])
+
+        return initial
+    }, {})
+}
 
 // export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join("%20")}&response _type=token&show_dialog=true`
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
