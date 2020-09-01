063ad49d374e923eb05f2314874d366d99b56f10
DJ <social.deej@gmail.com>
2020-09-02 01:10:35 +0530

    Adding Login Page Integrity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Login from './Login'
 
 function App() {
   return (
     <div className="app">
       <h1>My Mini World</h1>
+      {/* Spotify Logo */}
+      {/* Spotify Login */}
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <h1> Hello From Login Page </h1>
+            {/* Spotify Logo */}
+            {/* Login with Spotify Buttons*/}
+        </div>
+    )
+}
+
+export default Login 
