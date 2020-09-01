877e0d992ddb5732c61233bc90f2467971bd4f7c
DJ <social.deej@gmail.com>
2020-09-02 02:15:28 +0530

    Adding For Successful Redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  // Useful for Code Conditions // [] => Run Once , if [name] => Run Everytime Name Changes 
+  useEffect(() => {
+    const _token = getTokenFromUrl().access_token;
+    window.location.hash = "";
+
+    if (_token){
+      setToken (_token)
+    }
+
+    console.log(token)
+  }, []);
+
   return (
     <div className="app">
-      {/* Spotify Logo */}
-      {/* Spotify Login */}
-      <Login />
+      {
+          token?  (<h1>You are logged In </h1>) : (<Login/>)
+      }
     </div>
   );
 }
