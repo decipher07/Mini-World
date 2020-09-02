117404e3844cbd12cd6a659f02f8e73ae98ea5bf
DJ <social.deej@gmail.com>
2020-09-02 21:32:33 +0530

    Importing Reducer Registering unity

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,7 @@ const reducer = (state, action) => {
     console.log(action);
     switch(action.type){
         case 'SET_USER': return{...state, user: action.user}
-        default
+        default: return state 
     }
 }
 
