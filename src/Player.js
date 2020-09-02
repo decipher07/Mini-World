e0fed13ced61aaa1717ec633e6a6e0f8ab981c7e
DJ <social.deej@gmail.com>
2020-09-02 22:03:41 +0530

    Making Visual Case Divs

--- src/Player.js
+++ src/Player.js
@@ -3,10 +3,18 @@ import './Player.css'
 
 function Player({ spotify }) {
     return (
-        <div>
-            <h1>Welcome To Spotify</h1>
+        <div className="player">
+            <div className="player__body">
+                {/* Sidebar */}
+                {/* Body */}
+            </div>
+            {/* Footer */}
         </div>
     )
 }
+
+// Sidebar -- To be together 
+// Body -- 
+// Footer 
 
 export default Player ;
