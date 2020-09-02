b35c76ff5d57f050e0a5e3ff3e54fd6d22d01b9f
DJ <social.deej@gmail.com>
2020-09-02 22:56:52 +0530

    Replaced the Folder Commits

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Sidebar.css'
+import SidebarOption from './SidebarO'
 
 function Sidebar() {
     return(
         <div className="sidebar">
             <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="/" />
+            <SidebarOption option="Home"/>
         </div>
     )
 }
