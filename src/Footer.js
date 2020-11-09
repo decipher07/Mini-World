78ef0fa7e5800d72ba3b3974e4fd1083f32b9758
DJ <social.deej@gmail.com>
2020-11-09 18:25:07 +0530

    Footer for centering the stages

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,32 @@
 import React from "react";
-import './Footer.css'
+import './Footer.css';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+
+
 
 function Footer() {
     return(
         <div className="footer">
-            <h1>I am The Footer</h1>
+            <div className="footer__left">
+                <h1>Album and Song Details</h1>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon classname="footer__green" />
+                <SkipPreviousIcon classname="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon classname="footer__icon" />
+                <RepeatIcon classname="footer__green" />
+            </div>
+            <div className="footer__right">
+                <h1>Volume Controls</h1>
+            </div>
         </div>
     )
 }
